8b5268d839ddaf0b2b3e5fb348fe98f89a2fe054
Chip Turner <cturner@pattern.net>
2022-05-08 21:14:54 -0700

    use command helpers and tidy error handling

--- process_file_once/src/main.rs
+++ process_file_once/src/main.rs
@@ -1,37 +1,34 @@
 extern crate redis;
+use redis::Commands;
 use std::env;
 use std::fs;
 use std::process::Command;
 
 const KEY_NAME: &str = "process_once";
 
-fn main() -> Result<(), redis::RedisError> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let filename = fs::canonicalize(&args[1])
-        .unwrap()
+    let filename = fs::canonicalize(&args[1])?
         .into_os_string()
         .into_string()
         .unwrap();
     let params: Vec<String> = args[2..]
         .iter()
-        .map(|s| s.replace("{}", &filename.to_string()))
+        .map(|s| s.replace("{}", &filename))
         .collect();
     let args_string = args[2..].join("\0");
     let key = format!("{}\0{}", filename, args_string);
 
     let client = redis::Client::open("redis+unix:///var/run/redis/redis-server.sock")?;
     let mut con = client.get_connection()?;
-    let count: i64 = redis::cmd("SISMEMBER")
-        .arg(KEY_NAME)
-        .arg(&key)
-        .query(&mut con)?;
-    if count == 0 {
+
+    let count: bool = con.sismember(KEY_NAME, &key)?;
+    if !count {
         let output = Command::new(String::from(&params[0]))
             .args(params[1..].iter())
-            .output()
-            .expect("invoke worked");
+            .output()?;
         assert!(output.status.success());
-        redis::cmd("SADD").arg(KEY_NAME).arg(&key).query(&mut con)?;
+        con.sadd(KEY_NAME, &key)?;
     }
 
     Ok(())
